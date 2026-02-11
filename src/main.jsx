31de037aa6d47ba43481e43b64cf893d05896050
Danylo <danylohenrique2407@gmail.com>
2026-02-11 15:58:37 -0300

    fix(main.jsx): update import routes files

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
 
-import MainRoutes from './routes'
+import MainRoutes from './Routes'
 
 import './styles/main.scss'
 import { Footer, Header } from './components'
